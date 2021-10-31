6ccbe60edb9f1827ca7586b1df33cb2bada0fa68
Basir <basir.jafarzadeh@gmail.com>
2021-10-31 19:00:13 +0330

    add port

--- server.js
+++ server.js
@@ -36,6 +36,6 @@ app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/build/index.html'))
 );
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log('Server is ready at http://localhost:5000');
 });
